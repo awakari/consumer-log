f8f31834dc8722f2c433a02abe24ae7eb383ec79
Andrei Kurilov <andrei.kurilov@lekane.com>
2023-03-09 09:35:07 +0200

    refactor

--- service/service.go
+++ service/service.go
@@ -2,11 +2,11 @@ package service
 
 import (
 	"context"
-	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/event"
 )
 
 type Service interface {
-	Process(ctx context.Context, msg *cloudevents.Event) (err error)
+	Process(ctx context.Context, msg *event.Event) (err error)
 }
 
 type service struct {
@@ -16,6 +16,6 @@ func NewService() Service {
 	return service{}
 }
 
-func (svc service) Process(ctx context.Context, msg *cloudevents.Event) (err error) {
+func (svc service) Process(ctx context.Context, msg *event.Event) (err error) {
 	return // NOTE: This dummy implementation that does nothing.
 }
