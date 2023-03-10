901211ff8ca9cb29a683a603d3690de6cc862073
Andrei Kurilov <andrei.kurilov@lekane.com>
2023-03-10 10:46:51 +0200

    poll empty queue less frequently

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ type QueueConfig struct {
 	BatchSize          uint32 `envconfig:"QUEUE_BATCH_SIZE" default:"100" required:"true"`
 	Limit              uint32 `envconfig:"QUEUE_LIMIT" default:"1000" required:"true"`
 	Name               string `envconfig:"QUEUE_NAME" default:"consumer-log" required:"true"`
-	SleepOnEmptyMillis uint32 `envconfig:"QUEUE_SLEEP_ON_EMPTY_MILLIS" default:"100" required:"true"`
+	SleepOnEmptyMillis uint32 `envconfig:"QUEUE_SLEEP_ON_EMPTY_MILLIS" default:"1000" required:"true"`
 	SleepOnErrorMillis uint32 `envconfig:"QUEUE_SLEEP_ON_ERROR_MILLIS" default:"1000" required:"true"`
 	Uri                string `envconfig:"QUEUE_URI" default:"queue:8080" required:"true"`
 }
